b22acf28d60ed35ee33646518617ecc7d2dfd57d
Boyan  Pavlov <boyan.v.pavlov@gmail.com>
2024-02-21 18:16:03 +0200

    added background to the calendar

--- app/src/main/java/com/example/taskwise_eventmaster/presentation/home_page/BottomPartOfHomePage.kt
+++ app/src/main/java/com/example/taskwise_eventmaster/presentation/home_page/BottomPartOfHomePage.kt
@@ -1,5 +1,6 @@
 package com.example.taskwise_eventmaster.presentation.home_page
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
@@ -7,7 +8,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.taskwise_eventmaster.R
 import io.github.boguszpawlowski.composecalendar.StaticCalendar
 
 
@@ -20,6 +24,15 @@ fun CalendarPart(modifier: Modifier = Modifier) {
             .padding(7.dp)
             .background(color = Color.LightGray)
     ) {
+        val coverPhoto = painterResource(id = R.drawable.calendar_cover_photo)
+        Image(
+            painter = coverPhoto,
+            contentDescription = null,
+            contentScale = ContentScale.Crop, //used for scaling the image
+            alpha = 0.3f // used for changing the opacity of the image
+        )
+
+
         StaticCalendar()
     }
 }
